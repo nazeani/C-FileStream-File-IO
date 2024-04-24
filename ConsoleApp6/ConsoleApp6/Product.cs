a33c1e10c21ce2e3ec2f5cdf8a29ae3cad4672bb
nazeani <tu7pfrcay@code.edu.az>
2024-04-24 11:56:16 +0400

    task

---
+++ ConsoleApp6/ConsoleApp6/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    [Serializable]
+    public class Product : ISerializable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("id", Id,typeof(Product));
+            info.AddValue("name", Name, typeof(Product));
+            info.AddValue("description", Description, typeof(Product));
+        }
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Id = 1;
+            product.Name = "Test";
+            product.Description = "Test2";
+            ProductToJSON(product);
+            Product deserializedProduct = JsonToProduct();
+            Console.WriteLine($"Id={deserializedProduct.Id}, Name={deserializedProduct.Name}, Description={deserializedProduct.Description}");
+
+        }
+        public static void ProductToJSON(Product product)
+        {
+            string path = @"C:\Users\Nazani\OneDrive\Masaüstü\\datanazz.dat";
+            using Stream stream = new FileStream(path, FileMode.Create);
+            JsonSerializer.Serialize(stream, product);
+           
+        }
+        public static Product JsonToProduct() 
+        {
+            string path = @"C:\Users\Nazani\OneDrive\Masaüstü\\datanazz.dat";
+            using Stream stream = new FileStream(path, FileMode.Create);
+            Product product= JsonSerializer.Deserialize<Product>(stream);
+            return product;
+
+        }
+    }
+}
